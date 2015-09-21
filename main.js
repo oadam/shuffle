b89f0bf1af60850f63e5aca20c2a95864e5a49cd
x4d3 <moi@xade.eu>
2015-09-21 11:56:28 +0100

    Add an editable Title
    You may double click on the title to edit it. It is a breaking change on the database model.

--- main.js
+++ main.js
@@ -1,62 +1,95 @@
-var seed = new Date().toJSON().slice(0,10);
+var seed = new Date().toJSON().slice(0, 10);
 if (window.console) {
-	  console.log('using seed :' + seed);
+    console.log('using seed :' + seed);
 }
-                            
+
 var viewModel = {
-	  newValue: ko.observable(''),
-	    items: ko.observable([]),
-	      addItem: function() {
-		          var current = this.items().slice();
-			      current.push(this.newValue());
-			          this.newValue('');
-				      this.items(current);
-				        },
-	        removeItem: function(item) {
-			    var current = viewModel.items().slice();
-			        current = current.filter(function(i) {return i != item;});
-				    viewModel.items(current);
-				      },
-		  shuffledItems: ko.pureComputed(function() {
-			      var items = viewModel.items().slice();
-			          items = items.sort();
-				      var result = [];
-				          Math.seedrandom(seed);
-					      while (items.length) {
-						            var index = Math.floor(Math.random() * items.length);
-							          var removed = items.splice(index, 1);
-								        result.push(removed);
-									    }
-					          return result;
-						    })
+    title: ko.observable(),
+    //track whether we are editing the title
+    editingTitle: ko.observable(),
+    // edit the title
+    editTitle: function() {
+        this.editingTitle(true);
+    },
+    // stop editing the title. 
+    stopEditingTitle: function() {
+        this.editingTitle(false);
+    },
+    newValue: ko.observable(''),
+    items: ko.observable([]),
+    addItem: function() {
+        var current = this.items().slice();
+        current.push(this.newValue());
+        this.newValue('');
+        this.items(current);
+    },
+    removeItem: function(item) {
+        var current = viewModel.items().slice();
+        current = current.filter(function(i) {
+            return i != item;
+        });
+        viewModel.items(current);
+    },
+    shuffledItems: ko.pureComputed(function() {
+        var items = viewModel.items().slice();
+        items = items.sort();
+        var result = [];
+        Math.seedrandom(seed);
+        while (items.length) {
+            var index = Math.floor(Math.random() * items.length);
+            var removed = items.splice(index, 1);
+            result.push(removed);
+        }
+        return result;
+    })
 };
 
 //firebase
 var match = /\?firebaseId=([^&]*)/.exec(location.search);
 if (!match) {
-	  alert('firebaseId is needed');
+    alert('firebaseId is needed');
 }
 var firebaseId = decodeURIComponent(match[1]);
 var firebase = new Firebase('https://' + firebaseId + '.firebaseio.com');
 var firstValue = true;
+
+
 firebase.on('value', function(snapshot) {
-	  if (firstValue) {
-		      firstValue = false;
-		          $('*').show();
-			      $('.loading').hide();
-			        }
-	    viewModel.items(snapshot.val()||[]);
+    if (firstValue) {
+        firstValue = false;
+        $('.workspace').show();
+        $('.loading').hide();
+    }
+	var val = snapshot.val();
+	var title = "Double click to edit.";
+	var items = [];
+	if (val != null){
+		if (val.items != null){
+			items = val.items;
+		}
+		if (val.title != null){
+			title = val.title;
+		}
+	}
+	viewModel.items(items);
+	viewModel.title(title);
 });
 
 viewModel.items.subscribe(function(items) {
-	  firebase.set(items);
+	if (items){
+		firebase.update({'items':items});	
+	}
 });
-
+viewModel.title.subscribe(function(title) {
+	if (title){
+		firebase.update({'title':title});	
+	}
+});
 //init
 $(document).ready(function() {
-	  ko.applyBindings(viewModel);
-	    if (firstValue) {
-		        $('body').children().hide();
-			    $('.loading').show();
-			      }
+    ko.applyBindings(viewModel);
+    if (firstValue) {
+        $('.workspace').hide();
+        $('.loading').show();
+    }
 });
